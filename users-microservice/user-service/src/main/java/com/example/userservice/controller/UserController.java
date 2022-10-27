d44f5f6a599cf2cf911e31a4d256fc37144d4b22
qospwmf <qospwmf@gmail.com>
2022-10-27 14:30:30 +0900

    [user-service]⚡add : Greeting VO 추가해 Environment 대체

--- users-microservice/user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ users-microservice/user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.example.userservice.controller;
 
+import com.example.userservice.vo.Greeting;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 @RequestMapping("/")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final Environment env;
+
+    private final Greeting greeting;
 
     @GetMapping("/health_check")
     public String status() {
         return "It's working in user service";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+//        return env.getProperty("greeting.message");
+        return greeting.getMessage();
     }
 }
